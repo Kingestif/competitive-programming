b87b2dd1a575e88b5bd0730ac9fea9701dba5c64
estif <133001768+Kingestif@users.noreply.github.com>
2024-01-15 23:32:10 +0300

    Added solution for Count and Say

---
+++ leet-code-solutions/count-and-say.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return "1"
+        length=n ; n=str(1) 
+        for i in range(length-1):
+            s="" ; new=[]
+            for i in range(len(n)):
+                if i == 0:
+                    s+=n[i]
+                elif n[i]==n[i-1]:
+                    s+=n[i]
+                else:
+                    new.append(s)
+                    s=""
+                    s+=n[i]
+
+            if len(s) > 0:
+                new.append(s)
+            print(new)
+            ans=""
+
+            for i in range(len(new)):
+                ans+=(str(len(new[i]) )+ new[i][0])
+            n=ans
+            print(ans)
+        return ans
+
+        
