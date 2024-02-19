f25ff8a1e8ce5d4782648e97487f1146507e0bc2
estif <133001768+Kingestif@users.noreply.github.com>
2024-02-19 15:35:40 +0300

    Add solution for Car Pooling

---
+++ leet-code-solutions/leetcode/car-pooling.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        new=[0]*1002
+        for row in trips:
+            new[row[1]] += row[0]
+            new[row[2]] -= row[0]
+        print(new)
+        total=0
+        for i in range(len(new)):
+            total += new[i]
+            new[i] = total
+
+        for i in new:
+            if i > capacity:
+                return False
+        return True
+
+
+
+
+
