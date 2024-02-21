c2f1536a0f081c8416c16fe279836d7b2726d524
estif <133001768+Kingestif@users.noreply.github.com>
2024-02-21 14:53:24 +0300

    Add solution for Minimum Add to Make Parentheses Valid

---
+++ leet-code-solutions/leetcode/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        arr=[]
+        def push(x):
+            if x == "(":
+                arr.append(x)
+            else:
+                if len(arr) == 0:
+                    arr.append(x)
+                elif arr[-1] == "(":
+                    arr.pop()
+                else:
+                    arr.append(x)
+            # else:
+            #     if len(arr) == 0:
+            #         arr.append(x)
+            #     elif arr[-1] == "(":
+            #         arr.pop()
+            #     else:
+            #         arr.append(x)
+
+
+                
+        for i in range(len(s)):
+            push(s[i])
+            print(arr)
+
+        return len(arr)
+
+        
