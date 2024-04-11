569e6b781e5ef41ba5fe741a45b1156d7bf10510
estif <133001768+Kingestif@users.noreply.github.com>
2024-04-11 09:45:13 +0300

    Added solution for Broken Calculator

---
+++ leet-code-solutions/broken-calculator.py
@@ -0,0 +1,12 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        counter = 0
+        while target > startValue:
+            if target%2 == 0:
+                target //= 2
+            else:
+                target += 1
+            counter += 1
+        return counter + (startValue - target)
+            
+
