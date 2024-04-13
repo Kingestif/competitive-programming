709a5b09255bf54ad7e18910510af9828969693c
estif <133001768+Kingestif@users.noreply.github.com>
2024-04-13 12:07:11 +0300

    Added solution for Find the Maximum Divisibility Score

---
+++ leet-code-solutions/find-the-maximum-divisibility-score.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        dic = defaultdict(int)
+        for i in range(len(divisors)):
+            for j in nums:
+                if j%divisors[i] == 0:
+                    dic[(divisors[i],i)] += 1
+        print(dic)
+        if len(dic) <1:
+            return min(divisors)
+
+        val = max(dic.values())
+
+        ans = []
+        for key,values in dic.items():
+            if values == val:
+                ans.append(key[0])
+        print(ans)
+        return min(ans)
+        
