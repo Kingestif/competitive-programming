38cb8a44f4fa61d01a10e6ebd64a325a234fbfe5
estif <133001768+Kingestif@users.noreply.github.com>
2024-05-14 17:43:30 +0300

    Added solution for Course Schedule II

--- leet-code-solutions/course-schedule-ii.py
+++ leet-code-solutions/course-schedule-ii.py
@@ -1,35 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        map = defaultdict(list)
-        for course,pre in prerequisites:
-            map[course].append(pre)
-        ans = []
-        check = {}
+        incoming = [0 for i in range(numCourses+1)]   #we assume all courses have NO prerequest!
+        graph = defaultdict(list)   #we will append the courses that have prerequests to the PREREQUISITEE!!
+        queue = deque()
+        order = []
 
-        def DFS(node):
-            if node in check:
-                return check[node]
+        for x,y in prerequisites:
+            incoming[x] += 1    #for those who have prequest increment +1
+            graph[y].append(x)  #inorder to take x we need y so  
 
-
-            check[node] = True
-            neighbour = map[node]
-
-            for i in neighbour:
-                if DFS(i):
-                    return
-            check[node] = False
-            ans.append(node)
-
-            
+        for i in range(numCourses):  #if incoming is 0 it means we can process that(take that course) AND course that can be taken is appended to queue
+            if incoming[i] == 0:
+                queue.append(i)
 
 
+        # since we appended course that are availabe to take(those with incoming of 0) we can pop them then we decrement incoming of other ones so that they will be available when their incoming becomes 0
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                val = queue.popleft()
+                order.append(val)
 
+                neighbour = graph[val]
+                for course in neighbour:
+                    incoming[course] -= 1
 
+                    if incoming[course] == 0:
+                        queue.append(course)
 
+        if len(order) != numCourses:    #if there are courses that are not on queue means there is cyle 
+            return []
 
-        for i in range(numCourses):
-            if DFS(i):
-                return []
+        return order
 
-
-        return ans
+        
