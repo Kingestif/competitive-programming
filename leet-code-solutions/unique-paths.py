826a6fa7a9dcd3080e9adb7a5c9951357d6ceb08
estif <133001768+Kingestif@users.noreply.github.com>
2024-05-16 18:54:27 +0300

    Added solution for Unique Paths

--- leet-code-solutions/unique-paths.py
+++ leet-code-solutions/unique-paths.py
@@ -1,6 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = defaultdict(int)
         def inbound(x,y):  
             if x < m and y < n and x >= 0 and y >= 0:
                 return True
@@ -8,14 +7,18 @@ class Solution:
                 return False
 
 
-        def dp(x,y):
-            if not inbound(x,y):
-                return 0
-            if x == m-1 and y == n-1:
-                return 1
+        arr = [[0]*n for i in range(m)]
+        arr[m-1][n-1] = 1
 
-            if (x,y) not in memo:
-                memo[(x,y)] = dp(x+1,y) + dp(x,y+1)
-            return memo[(x,y)]
-            
-        return dp(0,0)
+        # we start from last then add 1 every time
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if inbound(i+1,j):
+                    arr[i][j] += arr[i+1][j]
+                if inbound(i,j+1):
+                    arr[i][j] += arr[i][j+1]
+                
+        return arr[0][0]
+
+
+        
